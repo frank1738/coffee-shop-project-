358b2a8898031f2cd59651d0c3cdedcb933f4123
frank1738 <franklineosoro08@gmail.com>
2022-06-04 11:54:47 +0300

    update readme file

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const menuBar = document.querySelector(".menu-bar");
-const menu = document.querySelector(".fa-bars");
-const switchToggle=document.querySelector(".toggle")
-const switchBtn=document.querySelector(".switch-btn")
-const Video=document.querySelector('.background-video')
-var playing=true
+const menuBar = document.querySelector('.menu-bar');
+const menu = document.querySelector('.fa-bars');
+const switchToggle = document.querySelector('.toggle');
+const switchBtn = document.querySelector('.switch-btn');
+const Video = document.querySelector('.background-video');
+var playing = true;
 switchBtn.addEventListener('click', Switch);
-menu.addEventListener("click", showBar);
+menu.addEventListener('click', showBar);
 function showBar() {
-  menuBar.classList.toggle("show")
+  menuBar.classList.toggle('show');
 }
 
-function Switch(){
-  switchToggle.classList.toggle('hide')
-  if(playing){
-    Video.pause()
-    playing=false
-  }
-  else {
-    Video.play()
-    playing=true
+function Switch() {
+  switchToggle.classList.toggle('hide');
+  if (playing) {
+    Video.pause();
+    playing = false;
+  } else {
+    Video.play();
+    playing = true;
   }
 }
